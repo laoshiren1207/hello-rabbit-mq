d598b72758acfebbe566cbf7eb2dcb99caa2588f
laoshiren <15207034473@163.com>
2020-10-05 22:58:50 +0800

    2020-10-05 22:56:00 简化代码，提取工具类。

--- hello-commons/src/main/java/com/laoshiren/hello/rabbit/commons/RabbitUtils.java
+++ hello-commons/src/main/java/com/laoshiren/hello/rabbit/commons/RabbitUtils.java
@@ -15,16 +15,30 @@ import com.rabbitmq.client.ConnectionFactory;
  */
 public class RabbitUtils {
 
+    /**
+     * 获取连接
+     * @return com.rabbitmq.client.Connection
+     * @throws java.io.IOException Exception
+     */
     public static Connection openConnection() throws Exception {
+        // 链接MQ工厂
         ConnectionFactory factory = new ConnectionFactory();
+        // 设置连接主机
         factory.setHost("120.79.0.210");
         factory.setPort(5672);
+        // 设置虚拟主机，
         factory.setVirtualHost("/");
         factory.setUsername("rabbit");
         factory.setPassword("123456");
         return factory.newConnection();
     }
 
+    /**
+     * 关闭资源
+     * @param channel   通道对象
+     * @param connection    连接
+     * @throws java.io.IOException Exception
+     */
     public static void closeResource(Channel channel,Connection connection) throws Exception {
         try {
             if (channel!=null )
