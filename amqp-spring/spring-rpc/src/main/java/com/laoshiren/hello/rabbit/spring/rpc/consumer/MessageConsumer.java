f86766757fe667e1c7c7c3f552fd24915d232f66
laoshiren <15207034473@163.com>
2021-05-24 17:07:48 +0800

    2021-05-24 17:07:44 amqp-rpc finish

--- amqp-spring/spring-rpc/src/main/java/com/laoshiren/hello/rabbit/spring/rpc/consumer/MessageConsumer.java
+++ amqp-spring/spring-rpc/src/main/java/com/laoshiren/hello/rabbit/spring/rpc/consumer/MessageConsumer.java
@@ -2,6 +2,7 @@ package com.laoshiren.hello.rabbit.spring.rpc.consumer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laoshiren.hello.rabbit.commons.json.JsonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Configuration;
@@ -24,17 +25,15 @@ import java.util.Map;
 @Configuration
 public class MessageConsumer {
 
-    @Resource
-    private ObjectMapper objectMapper;
 
     @RabbitListener(queues = QueueExchangeBean.QUEUE_NAME)
-    private String receiveMessage(String message) throws JsonProcessingException, InterruptedException {
+    private String receiveMessage(String message) throws InterruptedException {
         log.info(message);
         Map<String, String> result = new HashMap<>();
         result.put("code", "1");
         result.put("message", "操作成功");
         Thread.sleep(1000);
-        return objectMapper.writeValueAsString(result);
+        return JsonUtils.obj2json(result);
     }
 
 }
