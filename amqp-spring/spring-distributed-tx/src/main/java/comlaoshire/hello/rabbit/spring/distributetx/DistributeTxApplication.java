c07fdedda277facf1928fb4d8f3aa1385ce8dd61
laoshiren <15207034473@163.com>
2021-05-24 17:35:57 +0800

    2021-05-24 17:35:24 amqp分布式事务 准备

---
+++ amqp-spring/spring-distributed-tx/src/main/java/comlaoshire/hello/rabbit/spring/distributetx/DistributeTxApplication.java
@@ -0,0 +1,23 @@
+package comlaoshire.hello.rabbit.spring.distributetx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ProjectName:     hello-rabbit-mq
+ * Package:         comlaoshire.hello.rabbit.spring.distributetx
+ * ClassName:       DistributeTxApplication
+ * Author:          laoshiren
+ * Git:             xiangdehua@pharmakeyring.com
+ * Description:
+ * Date:            2021/5/24 17:32
+ * Version:         1.0.0
+ */
+@SpringBootApplication
+public class DistributeTxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributeTxApplication.class, args);
+    }
+
+}
